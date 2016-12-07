924f869d374e280af268a5558f2bd763ebfe774f
Paul Milne <hallhill@gmail.com>
2016-12-07 22:38:02 +0000

    wrote a makeRequest function to call in the main app function

--- public/app.js
+++ public/app.js
@@ -1,5 +1,13 @@
 var app = function(){
+     var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
+     makeRequest(url, requestComplete);
+}
 
+var makeRequest = function(url, callback){
+     var request = new XMLHttpRequest();
+     request.open("GET", url);
+     request.onload = callback;
+     request.send();
 }
 
 window.onload = app;
