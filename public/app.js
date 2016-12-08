31f2e4b0ef624e47cac4194beb974e9e5ed351e3
Paul Milne <hallhill@gmail.com>
2016-12-08 09:00:53 +0000

    getting album cover art

--- public/app.js
+++ public/app.js
@@ -26,8 +26,28 @@ var requestComplete = function(){
 }
 
 var populateList = function(albumList){
-     var albums = document.querySelector('#albums'); 
+     var albums = document.querySelector('#albums');
+     var albumsTable = document.querySelector('#albums_table') 
      for (eachAlbum of allAlbums) {
+
+          var row = document.createElement('tr');
+
+          var cell1 = document.createElement('td');
+          var coverArt = document.createElement('img');
+          coverArt.src = eachAlbum.images[1].url;
+          cell1.appendChild(coverArt);
+
+          var cell2 = document.createElement('td');
+          var br = document.createElement('br');
+          cell2.appendChild(eachAlbum.name)
+          cell2.appendChild(br)
+          cell2.appendChild(eachAlbum.artists[0].name);
+
+          row.appendChild(cell1);
+          row.appendChild(cell2);
+
+          albumsTable.appendChild(row);
+          
           var pTag = document.createElement('p');
           pTag.innerText = eachAlbum.name + " by " + eachAlbum.artists[0].name;
           albums.appendChild(pTag);
